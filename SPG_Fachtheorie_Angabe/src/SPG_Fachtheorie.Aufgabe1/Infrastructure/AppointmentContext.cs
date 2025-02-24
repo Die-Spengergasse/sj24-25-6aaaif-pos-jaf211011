7ac9c16e79febae6837e9819dd80e26748c5bedf
jaf211011 <144370516+jaf211011@users.noreply.github.com>
2025-02-24 18:05:00 +0100

    Solution for Exercise 1

---
+++ SPG_Fachtheorie_Angabe/src/SPG_Fachtheorie.Aufgabe1/Infrastructure/AppointmentContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SPG_Fachtheorie.Aufgabe1.Model;
+
+namespace SPG_Fachtheorie.Aufgabe1.Infrastructure
+{
+    public class AppointmentContext : DbContext
+    {
+        // TODO: Add your DbSets here
+        public DbSet<PaymentItem> PaymentItems => Set<PaymentItem>();
+        public DbSet<Payment> Payments => Set<Payment>();
+        public DbSet<CashDesk> CashDesks => Set<CashDesk>();
+        public DbSet<Employee> Employees => Set<Employee>();
+        public DbSet<Manager> Managers => Set<Manager>();
+        public DbSet<Cashier> Cashiers => Set<Cashier>();
+        public AppointmentContext(DbContextOptions options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .HasDiscriminator(e => e.Type);
+        }
+    }
+}
---
+++ SPG_Fachtheorie_Angabe/src/SPG_Fachtheorie.Aufgabe1/Model/Cashier.cs
@@ -0,0 +1,17 @@
+﻿namespace SPG_Fachtheorie.Aufgabe1.Model
+{
+    public class Cashier : Employee
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        protected Cashier() { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+        public Cashier(string jobSpezialisation, string firstName, string lastName, Address address)
+                       :base( firstName, lastName, address)
+        {
+            JobSpezialisation = jobSpezialisation;
+        }
+
+        public string JobSpezialisation { get; set; }
+    }
+}
